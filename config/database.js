fc535c0a2a295edf636c32d8cac4e3e4c981e0fb
Mallipudi Devi Siva Sai <sivasaimallipudi9h@gmail.com>
2025-12-06 22:17:53 +0530

    first commit

--- config/database.js
+++ config/database.js
@@ -1,14 +1,13 @@
 const { Sequelize } = require('sequelize');
-const dotenv = require('dotenv');
-
-dotenv.config();
+require('dotenv').config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME || 'ecommerce_db',
-    process.env.DB_USER || 'root',
-    process.env.DB_PASS || '',
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
     {
-        host: process.env.DB_HOST || 'localhost',
+        host: process.env.DB_HOST,
+        port: process.env.PORT,
         dialect: 'mysql',
         logging: false,
     }
