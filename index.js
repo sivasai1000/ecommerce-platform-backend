0074e36b12a6973d1f204fdf68e9161c61c56837
Mallipudi Devi Siva Sai <sivasaimallipudi9h@gmail.com>
2025-12-07 03:58:21 +0530

    first commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const path = require('path');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.DB_PORT;
+const PORT = process.env.PORT || 5000;
 
 const activityTracker = require('./middleware/activityTracker');
 // Middleware
