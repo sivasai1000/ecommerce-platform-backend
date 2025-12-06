61b6a0670f72698edfa6da13a43358be20ad490d
Mallipudi Devi Siva Sai <sivasaimallipudi9h@gmail.com>
2025-12-06 22:26:27 +0530

    first commit

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
     process.env.DB_PASS,
     {
         host: process.env.DB_HOST,
-        port: process.env.PORT,
+        port: process.env.DB_PORT,
         dialect: 'mysql',
         logging: false,
     }
